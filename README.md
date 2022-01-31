a0eddb80bf14025908af60338e3afefde6f780c0
Fernando Maia <fermaiasoares@aol.com>
2022-01-30 23:51:31 -0300

    Primeiro Pacote

---
+++ README.md
@@ -0,0 +1,26 @@
+# React Native Form JSON
+
+Create forms for React Native applications with schemas in JSON format.
+
+Save time when developing an application that needs to consume a form and send it to a Rest API or handle the data locally in the application.
+
+## User Guide
+### First Step
+
+Install with Yarn
+
+```shell
+  yarn add react-native-form-json
+```
+
+Or Npm
+
+```bash
+  npm install --save react-native-form-json
+```
+
+#### Examples
+
+See folder examples in [Repository]
+
+[Repository]: https://github.com/fermaiasoares/react-native-form-json
---
+++ index.tsx
@@ -0,0 +1,26 @@
+import { Form } from './src/components/Form/index';
+import { FormInput } from './src/components/Form/FormInput/index';
+import { FormCheckBoxSingle } from './src/components/Form/FormCheckBoxSingle/index';
+import { FormRadio } from './src/components/Form/FormRadio/index';
+import { FormSelect } from './src/components/Form/FormSelect/index';
+import { TextArea } from './src/components/TextArea/index';
+import { Input } from './src/components/Input/index';
+import { Select } from './src/components/Select/index';
+import { Radio } from './src/components/Radio/index';
+import { CheckBox } from './src/components/CheckBoxSingle/styles';
+import { IFormSchema, IFieldSchema } from './src/schemas/IFormSchema';
+
+export {
+  Form,
+  FormInput,
+  FormCheckBoxSingle,
+  FormRadio,
+  FormSelect,
+  TextArea,
+  Input,
+  Select,
+  Radio,
+  CheckBox,
+  IFormSchema,
+  IFieldSchema
+}
